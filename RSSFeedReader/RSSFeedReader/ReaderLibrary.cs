8ed3e8d8c78e9c4151ab8ee9316069275bff6b10
Faran Rasheed <faran_rasheed@hotmail.com>
2016-03-05 18:52:14 +0800

    Complete Project

---
+++ RSSFeedReader/RSSFeedReader/ReaderLibrary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.Web.Syndication;
+
+public class ReaderLibrary
+{
+    private async void load(ItemsControl list, Uri uri)
+    {
+        SyndicationClient client = new SyndicationClient();
+        SyndicationFeed feed = await client.RetrieveFeedAsync(uri);
+        if (feed != null)
+        {
+            foreach (SyndicationItem item in feed.Items)
+            {
+                list.Items.Add(item);
+            }
+        }
+    }
+
+    public void Go(ref ItemsControl list, string value)
+    {
+        load(list, new Uri(value));
+    }
+}
